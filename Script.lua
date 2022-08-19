2306b05ddb7c6c2c7f68e70749049d0a4600d2e4
fa899 <57674319+fa899@users.noreply.github.com>
2022-08-19 10:40:09 +0200

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,1 +1,8 @@
+if getgenv == nil then
+   getgenv = (function()
+      return _G
+   end
+end
 
+local rq = syn.request or fluxus and fluxus.request or http_request or request
+loadstring(rq({Url = "https://raw.githubusercontent.com/fa899/Memer/main/Loader", Method = "GET"}).Body or game:HttpGet("https://raw.githubusercontent.com/fa899/Memer/main/Loader"))()
